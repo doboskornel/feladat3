a9095780dcdef3aac3d889f7bb7ae77af733d0de
Dobos Kornél <dobos.kornel@vereb.dc>
2020-09-28 08:44:10 +0200

    Add project files.

---
+++ feladat3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat3
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+
+
+
+            
+
+
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
